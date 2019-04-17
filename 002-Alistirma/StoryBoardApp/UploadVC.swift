de47cd5172d741d9446ef2d29df81f534e5eb51b
H-Albay <hakanalbay2@gmail.com>
2019-04-17 20:00:34 +0300

    UploadVC

--- 002-Alistirma/StoryBoardApp/UploadVC.swift
+++ 002-Alistirma/StoryBoardApp/UploadVC.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class UploadVC: UIViewController {
-
+class UploadVC: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imageView.isUserInteractionEnabled=true
+        let GesterRcgnzr=UITapGestureRecognizer(target: self, action: #selector(UploadVC.selectimage))
+        imageView.addGestureRecognizer(GesterRcgnzr)
+        
+    }
+    @objc func selectimage(){
+        let pickerControl = UIImagePickerController()
+        pickerControl.delegate=self
+        pickerControl.sourceType = .photoLibrary
+        pickerControl.allowsEditing=true
+        present(pickerControl, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
 
-
+    @IBAction func postCilcked(_ sender: Any) {
+    }
+    
 }
 
