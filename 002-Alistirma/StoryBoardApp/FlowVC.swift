e17ae151be6c5a6e9c2f30c19abca89f533371d4
H-Albay <hakanalbay2@gmail.com>
2019-04-20 13:33:33 +0300

    DataViews

--- 002-Alistirma/StoryBoardApp/FlowVC.swift
+++ 002-Alistirma/StoryBoardApp/FlowVC.swift
@@ -10,8 +10,11 @@ import UIKit
 import  Firebase
 import FirebaseAuth
 class FlowVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
     @IBOutlet weak var tableView: UITableView!
+    var UserEmailArry = [String] ()
+    var UserImageArry = [String]()
+    var UserCommentArry = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,19 +38,25 @@ class FlowVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             for id in postId {
                 let singlePost = post[id] as! NSDictionary
                 
-                let posttext = singlePost ["PostText"] as! String
-                print(posttext)
+                //let posttext = singlePost ["PostText"] as! String
+                //print(posttext)
+                self.UserEmailArry.append(singlePost["PostedBy"] as! String)
+                self.UserCommentArry.append(singlePost["PostText"] as! String)
             }
+            self.tableView.reloadData()
             
         }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return UserEmailArry.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for:
             indexPath ) as! FlowTVCell
+        
+        cell.UserEmailLabel.text = UserEmailArry [indexPath.row]
+        cell.UserComment.text = UserCommentArry [indexPath.row]
         return cell
     }
 
