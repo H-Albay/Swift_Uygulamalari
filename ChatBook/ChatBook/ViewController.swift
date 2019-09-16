364a11692d87ec537d844b75f48c9c55389a839c
H-Albay <hakanalbay2@gmail.com>
2019-09-16 23:41:54 +0300

    Commit-014

---
+++ ChatBook/ChatBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatBook
+//
+//  Created by Hakan Albay on 16.09.2019.
+//  Copyright © 2019 Hakan Albay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
