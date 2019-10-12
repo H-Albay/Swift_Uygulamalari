7e75f22db80cb8bb5343219fadc81882235ea2c8
H-Albay <hakanalbay2@gmail.com>
2019-10-12 22:04:19 +0300

    Basic Upload

--- ChatBook/ChatBook/ViewConrollers/YükleVC.swift
+++ ChatBook/ChatBook/ViewConrollers/YükleVC.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 class Yu_kleVC: UIViewController ,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var uploadImageView: UIImageView!
@@ -20,6 +20,44 @@ class Yu_kleVC: UIViewController ,UIImagePickerControllerDelegate,UINavigationCo
        
     }
     @IBAction func uploadClicked(_ sender: Any) {
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        let mediaFolder = storageRef.child("Medya")
+        if let data = uploadImageView.image?.jpegData(compressionQuality: 0.5) {
+            let uuid = UUID().uuidString
+            let imageRef = mediaFolder.child("\(uuid).jpg")
+            imageRef.putData(data, metadata: nil) { (metadata, error) in
+                if error != nil {
+                    self.makeAlert(title: "Hata", message: error?.localizedDescription ?? "Hata")
+                }else{
+                    imageRef.downloadURL { (url, error) in
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                            let fireStore = Firestore.firestore()
+                            let medyahakkında = ["URL" : imageUrl!,"Medya Kullanıcısı": UserSingleton.sharedUserInfo.kullanıcıadı,"Tarih": FieldValue.serverTimestamp()] as [String : Any]
+                            fireStore.collection("Medyalar").addDocument(data: medyahakkında) { (error) in
+                                if error != nil {
+                                    self.makeAlert(title: "Hata", message: error?.localizedDescription ?? "Hata")
+                                } else {
+                                    //Tabbarda indexleme mevcuttur burada anasayfa gidicek.
+                                    self.tabBarController?.selectedIndex = 0
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            
+        }
+        
+    }
+    func makeAlert (title: String,message: String){
+        let alert = UIAlertController.init(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction.init(title: "Tamam", style: .default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
     }
     @objc func choosePicture () {
         let picker = UIImagePickerController()
@@ -31,7 +69,8 @@ class Yu_kleVC: UIViewController ,UIImagePickerControllerDelegate,UINavigationCo
         uploadImageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
     }
-    
-
+  
 
 }
+
+
